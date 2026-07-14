5cf98cb6f041217b4a70e0652ec9d66be2d12b41
Huakun Shen <huakun.shen@huakunshen.com>
2026-07-14 17:23:43 +0800

    feat(tui-charts): renderHistogram (binning over renderBarChart)

--- packages/tui-charts/src/index.ts
+++ packages/tui-charts/src/index.ts
@@ -1,2 +1,4 @@
 export { renderBarChart } from "./bar";
 export type { BarDatum, BarChartOptions } from "./bar";
+export { renderHistogram } from "./histogram";
+export type { HistogramOptions } from "./histogram";
